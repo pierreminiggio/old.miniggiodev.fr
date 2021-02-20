7e0c8ef01b1c3fef3ba0c91fd61b1a4918f80bdb
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-02-20 16:29:43 +0100

    return json

--- api/getVideoableLikes.php
+++ api/getVideoableLikes.php
@@ -33,6 +33,9 @@ $likes = array_map(fn (array $entry) => [
         ->createQuery('social__youtube')
         ->select('id', 'youtubeid', 'title', 'channel_id')
         ->where('channel_id IS NOT NULL AND videoed_at IS NULL')
+        ->orderBy('created_at ASC')
 ));
-var_dump($likes);
+
+http_response_code(200);
+echo json_encode($likes);
 exit;
