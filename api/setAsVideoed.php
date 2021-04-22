f2e7303fde64a36709786a86e55bede9d9cd070d
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-04-23 01:38:01 +0200

    validate body not empty

--- api/setAsVideoed.php
+++ api/setAsVideoed.php
@@ -11,4 +11,18 @@ BearerTokenChecker::check();
 $fetcher = (new DatabaseFetcherFactory())->make(DatabaseConnection::UTF8_MB4);
 
 $requestBody = file_get_contents('php://input');
-var_dump($requestBody);
+
+if (empty($requestBody)) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Body can\'t be empty']);
+    exit;
+}
+
+$jsonRequestBody = json_decode($requestBody);
+if (empty($jsonRequestBody)) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Bad JSON body']);
+    exit;
+}
+
+var_dump($jsonRequestBody);
