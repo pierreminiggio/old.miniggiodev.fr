2ed50b8728d2967a08ffddffcab953bae87f2199
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2022-08-20 23:03:20 +0200

    offset

--- api/getTweets.php
+++ api/getTweets.php
@@ -13,6 +13,28 @@ $fetcher = (new DatabaseFetcherFactory())->make(DatabaseConnection::UTF8_MB4);
 
 $today = new DateTime();
 
+$offset = $_GET['offset'] ?? null;
+
+$query = $fetcher->createQuery(
+    'social__publication'
+)->select(
+    'id_publication_source as status_id',
+    'in_reply_to_status_id as in_reply_to_status_id',
+    'texte_brut',
+    'texte_html',
+    'date_publication'
+)->orderBy(
+    'id_publication_source'
+)->limit(
+    20
+);
+$queryParams = [];
+
+if ($offset) {
+    $query = $query->where('id_publication_source > :status_id');
+    $queryParams['status_id'] = $offset;
+}
+
 $tweets = array_map(
     fn (array $entry) => [
         'status_id' => $entry['status_id'],
@@ -23,24 +45,8 @@ $tweets = array_map(
         'texte_html' => $entry['texte_html'],
         'date_publication' => $entry['date_publication']
     ],
-    $fetcher->query(
-        $fetcher->createQuery(
-            'social__publication'
-        )->select(
-            'id_publication_source as status_id',
-            'in_reply_to_status_id as in_reply_to_status_id',
-            'texte_brut',
-            'texte_html',
-            'date_publication'
-        )/*->where(
-            ''
-        )*/->orderBy(
-            'id_publication_source'
-        )->limit(
-            50
-        )/*,
-        ['today_at_midnight' => $today->format('Y-m-d') . ' 00:00:00']*/
-));
+    $fetcher->query($query, $queryParams)
+);
 
 http_response_code(200);
 echo json_encode($tweets);
